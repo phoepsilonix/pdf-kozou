45fcbee9a910716ac8af3be5de67167eb740ae7e
Masato TOYOSHIMA <phoepsilonix@phoepsilonix.love>
2026-03-25 17:44:59 +0900

    npm update

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import "pdfjs-dist/web/pdf_viewer.css";
+//import "pdfjs-dist/web/pdf_viewer.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
