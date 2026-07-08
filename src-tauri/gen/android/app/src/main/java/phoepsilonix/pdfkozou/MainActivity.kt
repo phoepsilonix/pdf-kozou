8d594de4bca3bd74811d41a9586cf8e26490eaf6
Masato TOYOSHIMA <phoepsilonix@phoepsilonix.love>
2026-07-08 20:32:13 +0900

    android

---
+++ src-tauri/gen/android/app/src/main/java/phoepsilonix/pdfkozou/MainActivity.kt
@@ -0,0 +1,11 @@
+package phoepsilonix.pdfkozou
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+
+class MainActivity : TauriActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    enableEdgeToEdge()
+    super.onCreate(savedInstanceState)
+  }
+}
